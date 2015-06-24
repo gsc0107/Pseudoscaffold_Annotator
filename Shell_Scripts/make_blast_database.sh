ac37285242cffec9cc6a3fed823fa60617ff40d3
Paul Hoffman <hoffmanp@agro200953239.cfans.umn.edu>
2015-06-24 18:14:48 -0500

    Added more BLAST utilities

---
+++ Shell_Scripts/make_blast_database.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+#   Test to make sure NCBI's BLAST+ is installed
+#   specifically `makeblastdb`
+
+if `command -v makeblastdb > /dev/null 2> /dev/null`
+then
+    echo "BLAST+ installed"
+else
+    echo "You need NCBI's BLAST+ utilities installed and in your PATH"
+    exit 1
+fi
+
+INPUT_PSEUDOSCAFFOLD="$1"
+OUTPUT_DATABASE="$2"
+
+makeblastdb -in ${INPUT_PSEUDOSCAFFOLD} -dbytpe nucl -out ${OUTPUT_DATABASE}
