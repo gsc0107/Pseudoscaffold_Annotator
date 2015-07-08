2742b3522f5e41871feea1923a2ac4a676df5ac4
Paul Hoffman <hoffmanp@agro200953239.cfans.umn.edu>
2015-07-08 14:38:45 -0500

    Minor fixes to argument passing

--- Miscellaneous_Utilities/blast_utilities.py
+++ Miscellaneous_Utilities/blast_utilities.py
@@ -61,7 +61,7 @@ def make_blast_database(bconf, shellpath, pseudoscaffold):
         sys.exit("Incorrect BLAST database type specified")
     database_script = str(shellpath + '/make_blast_database.sh')
     database_args = [pseudoscaffold, database_name, database_type, ext]
-    database_cmd = ['bash', database_script, database_args]
+    database_cmd = ['bash', database_script, str(database_args)]
     database_shell = subprocess.Popen(database_cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = database_shell.communicate()
     #return(database_name, out, err)
