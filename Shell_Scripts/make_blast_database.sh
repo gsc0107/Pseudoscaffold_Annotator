16beaccb21961ae1fb121e186ada82886d26b7f0
Paul Hoffman <hoffmanp@agro200953239.cfans.umn.edu>
2015-07-09 09:11:23 -0500

    Fixes to making BLAST db

--- Shell_Scripts/make_blast_database.sh
+++ Shell_Scripts/make_blast_database.sh
@@ -22,4 +22,4 @@ INPUT_PSEUDOSCAFFOLD="$1"
 OUTPUT_DATABASE="$2"
 DB_TYPE="$3"
 
-makeblastdb -dbytpe "${DB_TYPE}" -in "${INPUT_PSEUDOSCAFFOLD}" -out "${OUTPUT_DATABASE}"
+makeblastdb -dbtype "${DB_TYPE}" -in "${INPUT_PSEUDOSCAFFOLD}" -out "${OUTPUT_DATABASE}"
