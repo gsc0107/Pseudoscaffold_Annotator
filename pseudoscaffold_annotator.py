8c2bb018880bf47f2b1f322c3312a6ca9c7a2521
Paul Hoffman <hoff0792@umn.edu>
2015-07-31 09:43:39 -0500

    Tests

--- pseudoscaffold_annotator.py
+++ pseudoscaffold_annotator.py
@@ -54,10 +54,11 @@ def pseudoscaffold_annotator(args, temppath, rootpath, shellpath, pseudopath):
             out.append(str(unique + '_out.gff'))
         #   Set up a list of arguments
         args = zip(itertools.repeat(seq_list), contig_original, itertools.repeat(annotation), itertools.repeat(pseudoscaffold), out, itertools.repeat(temppath), itertools.repeat(bconf), itertools.repeat(database_name), itertools.repeat(pseudopath))
-        if __name__ == '__main__':
-            pool = Pool()
-            #pool.map(gff_to_gff.gff_to_gff, args)
-            pool.map(wrapper, args)
+        # if __name__ == '__main__':
+        #     pool = Pool()
+        #     #pool.map(gff_to_gff.gff_to_gff, args)
+        #     pool.map(wrapper, args)
+        return args
     elif find_gff and create_bed:
         print "Found GFF file, making BED file"
         import GFF_Utilities.gff_to_bed as gff_to_bed
